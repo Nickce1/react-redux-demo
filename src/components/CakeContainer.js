ca2b9dea02fc9b1c3dd5b9191c98f209f88bb237
Nickce1 <f.lineup@gmail.com>
2020-09-24 10:15:29 +0400

    react redux demo in a tutorial

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { buyCake } from '../redux/index'
+
+function CakeContainer(props) {
+  return (
+    <>
+      <p>Number of cakes: {props.numOfCakes}</p>
+      <button onClick={props.buyCake}>BUY CAKE</button>
+    </>
+  )
+}
+
+const mapStateToProps = state => ({
+  numOfCakes: state.numOfCakes
+})
+
+const mapDispatchToProps = dispatch => {
+  return {
+    buyCake: () => dispatch(buyCake())
+  }
+}
+
+export default connect (mapStateToProps, mapDispatchToProps)(CakeContainer)
---
+++ src/components/HooksCakeContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { buyCake } from '../redux/index'
+
+function HooksCakeContainer() {
+  const numOfCakes = useSelector(state => state.numOfCakes)
+  const dispatch = useDispatch()
+
+  return (
+    <>
+      <h2>Num of cakes: {numOfCakes}</h2>
+      <button onClick={() => {dispatch(buyCake())}}>BUY CAKE</button>
+    </>
+  )
+}
+
+export default HooksCakeContainer
